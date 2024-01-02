01fd0afd00aace902859d92c40502e5e13c50147
Ruchhit <ruchitgule2219@gmail.com>
2024-01-02 21:54:36 +0530

    update: file uploadation through multer

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -3,4 +3,9 @@ import { asyncHandler } from "../Utils/asyncHandler.js";
 const userController = asyncHandler((req,res)=>{
     res.status(200).json({message:"Hello World"})
 });
+
+const registerUser = asyncHandler((req,res)=>{
+    const [userName,email,password,fullName,avatar,coverImage] = req.body;
+})
+
 export {userController}
--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,7 +1,18 @@
  import {Router} from "express";
 const router = Router();
 import { userController } from "../Controllers/user.controller.js";
+import { upload } from "../Middlewares/multer.middleware.js";
 
-router.route("/register").post(userController);
+router.route("/register").post(upload.fields([
+    {
+    name: "avatar",
+    maxCount: 1
+    },
+    {
+        name: "coverImage",
+        maxCount: 1
+    }
+    
+]),userController);
 
 export {router}
