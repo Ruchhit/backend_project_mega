0b979aa4f487851a7712e7b0f9c9b6f1b8789b2c
Ruchhit <ruchitgule2219@gmail.com>
2024-01-02 22:14:53 +0530

    checking if user already existed

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,16 +1,27 @@
+import { User } from "../Models/user.model.js";
 import { asyncHandler } from "../Utils/asyncHandler.js";
 
 const userController = asyncHandler((req,res)=>{
     res.status(200).json({message:"Hello World"})
 });
 
-const registerUser = asyncHandler((req,res)=>{
+const registerUser = asyncHandler(async (req,res)=>{
     const {userName,email,password,fullName,avatar,coverImage} = req.body;
 
     if([userName,email,password,fullName,avatar,coverImage].some((field)=> field?.trim() === ""))
 {
     return res.status(400).json({message:"Please fill all the fields"})
 }
+
+ const existedUser = await User.findOne({$or : [{userName} , {email}]})
+ if(existedUser){
+     return res.status(400).json({message:"User already exists"})
+ }
+
+
+
+
+
 })
 
  
