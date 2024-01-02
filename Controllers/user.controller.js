d7673076657316580d6ca199a5da3f902ae0a246
Ruchhit <ruchitgule2219@gmail.com>
2024-01-02 22:00:15 +0530

    update: validation if all field are thr

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -5,7 +5,14 @@ const userController = asyncHandler((req,res)=>{
 });
 
 const registerUser = asyncHandler((req,res)=>{
-    const [userName,email,password,fullName,avatar,coverImage] = req.body;
+    const {userName,email,password,fullName,avatar,coverImage} = req.body;
+
+    if([userName,email,password,fullName,avatar,coverImage].some((field)=> field?.trim() === ""))
+{
+    return res.status(400).json({message:"Please fill all the fields"})
+}
 })
 
-export {userController}
+ 
+
+export {userController,registerUser}
