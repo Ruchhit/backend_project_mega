bf6cd2ed13865c0d180187fe312e96a6f3f883b2
Ruchhit <ruchitgule2219@gmail.com>
2024-01-03 15:21:13 +0530

    update : testing done ,even multer uploaded image in server

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,14 +1,16 @@
 import { User } from "../Models/user.model.js";
 import { asyncHandler } from "../Utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../Utils/cloudinary.js"; 
+
 
 const userController = asyncHandler((req,res)=>{
-    res.status(200).json({message:"Hello World"})
+    res.status(200).json({message:"sab sahi chal rha hai"})
 });
 
 const registerUser = asyncHandler(async (req,res)=>{
-    const {userName,email,password,fullName,avatar,coverImage} = req.body;
+    const {userName,email,password,fullName} = req.body;
 
-    if([userName,email,password,fullName,avatar,coverImage].some((field)=> field?.trim() === ""))
+    if([userName,email,password,fullName ].some((field)=> field?.trim() === ""))
 {
     return res.status(400).json({message:"Please fill all the fields"})
 }
@@ -17,6 +19,37 @@ const registerUser = asyncHandler(async (req,res)=>{
  if(existedUser){
      return res.status(400).json({message:"User already exists"})
  }
+
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+if(!avatarLocalPath)
+{
+    res.send(400).status({message : "avatar is must!!!"})
+}
+
+const avatar = await uploadOnCloudinary(avatarLocalPath)
+const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+const user = await User.create({
+    userName,
+    email,
+    password,
+    fullName,
+    avatar : avatar?.url,
+    coverImage: coverImage?.url || null,
+})
+//checknng wheather user created or not
+const createdUser = await User.findById(user._id)
+
+const validUser = await User.findById(user._id).select("-password -accessToken");
+if(!validUser)
+{
+    return res.status(400).json({message:"Something went wrong"})
+}
+
+return res.status(200).json(validUser)
+
 
 
 
